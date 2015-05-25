04b19dd5f9c4a3006b146c0799c8ab90fc85d529
AITERRAMI Mohammed Amine <crysis473@gmail.com>
2015-05-25 02:45:38 +0200

    Handling the value to be displayed on the screen (#2).

--- src/com/calculator/Engine.java
+++ src/com/calculator/Engine.java
@@ -10,6 +10,38 @@ package com.calculator;
  */
 public class Engine {
 
+    // The symbole of the operation to preform.
+    private char operationSymbol;
+    // The result of the operation.
+    private int valueOnScreen;
+    // The operand of the operation.
+    private int operand;
+
+    /**
+     * Instanciates an Engine.
+     */
+    public Engine() {
+        valueOnScreen = 0;
+        operationSymbol = ' ';
+        operand = 0;
+    }
+
+    /**
+     * @return The result of the operation or "0" if no operation has been preformed yet.
+     */
+    public int getValueOnScreen() {
+        return valueOnScreen;
+    }
+
+    // Ok for the commit.
+
+    /**
+     * Number button pressed.
+     * @param number The on digit number related to the pressed button.
+     */
+    public void numberPressed(int number) {
+        valueOnScreen = valueOnScreen*10 + number;
+    }
 
 }
 
