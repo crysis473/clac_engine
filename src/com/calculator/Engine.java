5d8957925023dff92a91b5754edf5966b9ae41eb
AITERRAMI Mohammed Amine <crysis473@gmail.com>
2015-05-25 02:35:57 +0200

    [add] Engine.java (#2)

---
+++ src/com/calculator/Engine.java
@@ -0,0 +1,15 @@
+package com.calculator;
+
+/**
+ * Created by user on 25/05/2015.
+ */
+
+/**
+ * Handles and preforms arithmetical operations entered by user via the command line or/and the
+ * graphical interface.
+ */
+public class Engine {
+
+
+}
+
